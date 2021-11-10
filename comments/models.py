af8db5b18c3c4f4ff64f57178f875a8ff253f341
bhagavath <lordcodeking@gmail.com>
2021-11-10 10:39:54 +0530

    changed comments model

--- comments/models.py
+++ comments/models.py
@@ -3,11 +3,11 @@ from profiles.models import user
 from books.models import book
 # Create your models here.
 class comment(models.Model):
-    book = models.OneToOneField(book, models.DO_NOTHING, primary_key=True)
-    user = models.ForeignKey(user, models.DO_NOTHING)
+    comment_id = models.AutoField(primary_key=True)
+    book = models.ForeignKey(book, models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(user, models.DO_NOTHING, blank=True, null=True)
     comment = models.TextField(blank=True, null=True)
 
     class Meta:
         managed = False
         db_table = 'comments'
-        unique_together = (('book', 'user'),)
